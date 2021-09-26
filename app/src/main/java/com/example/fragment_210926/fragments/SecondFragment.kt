e34a06b6ab459bafd0122accd1fa8d1b7b5b06f5
이진호 <spris1513@gmail.com>
2021-09-26 12:29:03 +0900

    토스트 띄우기 - requireContext() 활용

--- app/src/main/java/com/example/fragment_210926/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/fragment_210926/fragments/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.fragment_210926.MainActivity
 import com.example.fragment_210926.R
 import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.fragment_second.*
@@ -25,7 +26,11 @@ class SecondFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         fragToastBtn.setOnClickListener {
 
-//            Toast.makeText(this, "버튼눌림", Toast.LENGTH_SHORT).show()
+//            코드적는곳:부품
+//            토스트의 재료로 필요한것:화면
+//            화면을 불러내려면??
+
+            Toast.makeText(requireContext(), "버튼눌림", Toast.LENGTH_SHORT).show()
             Log.d("두번째부품","버튼눌림")
         }
     }
