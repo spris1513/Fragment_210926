74b90f83b11e8255dc9c307002100bac7776e3ec
이진호 <spris1513@gmail.com>
2021-09-26 12:04:22 +0900

    프래그먼트의 독장 코드 - onActivityCreated 활용
    전에 FirstFragment 활용건누락

---
+++ app/src/main/java/com/example/fragment_210926/fragments/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.fragment_210926.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragment_210926.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        logBtn.setOnClickListener {
+            Log.d("첫부품","버튼눌림")
+        }
+
+    }
+
+}
